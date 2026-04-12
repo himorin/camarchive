81eb157f9167a62899806dbbcb4d0246d45d42fc
Atsushi Shimono <atsushi@himor.in>
2026-04-13 00:34:37 +0900

    added comment for scripts

--- scripts/scripts_defs.py
+++ scripts/scripts_defs.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
+# NOTE: sync to webapi/PNAPI/Constants.pm
+
 DEF_CONF_NAME = "../common/config.json"
+# keep format compatible with number (e.g. not include hyphen) for easy sorting to find nearby file(s) from any time only by lt/gt comparison
 DEF_FMT_DATE = "%Y%m%d"
 DEF_FMT_FULL = "%Y%m%d%H%M%S"
 DEF_FLIST = "imglist"
